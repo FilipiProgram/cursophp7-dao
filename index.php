de265b15a7f5f88852e00dd2c86ad4fd75cb14f7
FilipiProgram <Filipisf2013@gmail.com>
2020-02-08 23:05:48 -0300

    Inserir e Alterar

--- index.php
+++ index.php
@@ -10,15 +10,26 @@ require_once("config.php");
 //$lista = Usuario::getList();
 //echo json_encode($lista);
  
- //Carrega uma lista de usuários buscando pelo Login
-
+//Carrega uma lista de usuários buscando pelo Login
 //$search = Usuario::search("ri");
 //echo json_encode($search);
 
 //Carrega um usuário usando o Login e a Senha
+//$usuario = new Usuario();
+//$usuario->login("ricardo", "12233111");
+//echo $usuario;
+
+//Criando um novo usuario
+//$aluno = new Usuario("Aluninho", "2758");
+//$aluno->insert();
+//echo $aluno;
 
 $usuario = new Usuario();
-$usuario->login("ricardo", "12233111");
 
-echo $usuario;
+$usuario->loadbyId(8);
+
+$usuario->update("professor", "akrllll");
+
+echo $usuario
+
  ?>
